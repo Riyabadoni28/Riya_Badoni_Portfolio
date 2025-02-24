0dca3962044d0ae996c78af0f174fb1747004035
riya badoini <riya.badoni@logicladder.com>
2025-02-24 15:52:22 +0530

    add style

--- src/app/project/component/home/home.component.ts
+++ src/app/project/component/home/home.component.ts
@@ -1,18 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { GoogleMapsModule } from '@angular/google-maps';
 
 @Component({
   selector: 'app-home',
   standalone: true, // Marks it as standalone
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  imports: [CommonModule, ], // Imports necessary modules
+  imports: [CommonModule, GoogleMapsModule, ], // Imports necessary modules
 })
 export class HomeComponent implements OnInit {
+
+    center: google.maps.LatLngLiteral = { lat: 40.7128, lng: -74.0060 }; // New York
+    zoom = 12;
 
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }
--- src/app/project/layout/header/nav/nav.component.ts
+++ src/app/project/layout/header/nav/nav.component.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { GoogleMapsModule } from '@angular/google-maps';
 import { RouterModule } from '@angular/router';
 
 declare const google: any;
@@ -8,7 +7,7 @@ declare const google: any;
 @Component({
     selector: 'app-nav',
     standalone: true, // Marks it as standalone
-    imports: [CommonModule,GoogleMapsModule, RouterModule], // Imports necessary modules
+    imports: [CommonModule,RouterModule], // Imports necessary modules
     templateUrl: './nav.component.html',
     styleUrl: './nav.component.scss'
 })
@@ -16,6 +15,5 @@ declare const google: any;
 
 export class NavComponent {
 
-    center: google.maps.LatLngLiteral = { lat: 40.7128, lng: -74.0060 }; // New York
-    zoom = 12;
+
 }
